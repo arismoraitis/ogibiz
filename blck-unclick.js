f66d37cae15b0460bc6ec7cc4523a775b17f68c9
arismoraitis <105006025+arismoraitis@users.noreply.github.com>
2024-07-29 19:35:54 +0300

    Update blck-unclick.js

--- blck-unclick.js
+++ blck-unclick.js
@@ -1,7 +1,10 @@
-var element = document.getElementById("slider-home-0");
-
-// Check if the element exists
-if (element) {
-    // Remove the 'mouseover' event listener from the element
-    element.removeEventListener("mouseover", yourFunction, false);
+function removeMouseOverListener() {
+    // Select the element with the ID 'slider-home-0'
+    var element = document.getElementById("slider-home-0");
+    
+    // Check if the element exists
+    if (element) {
+        // Remove the 'mouseover' event listener from the element
+        element.removeEventListener("mouseover", removeMouseOver, false);
+    }
 }
