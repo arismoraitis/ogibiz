8d315d5c7aff129b13b4fb5092ea6a87e47db048
arismoraitis <105006025+arismoraitis@users.noreply.github.com>
2024-07-29 22:28:47 +0300

    Update blck-unclick.js

--- blck-unclick.js
+++ blck-unclick.js
@@ -1,9 +1,38 @@
-$('#slider-home-0').on('click', 'a', function(event) {
-    // Check if the target is a link
-    if ($(event.target).is('a')) {
-        return true; // Allow the link to be followed
+(function() {
+    // Get the element
+    var element = document.getElementById('slider-home-0.carousel.bs-slider.slide.control-round.indicators-line');
+
+    // Store the existing event handler
+    var oldHandler;
+
+    // Function to find and remove old event listener
+    function findAndRemoveOldListener() {
+        var listeners = getEventListeners(element);
+        if (listeners && listeners.mouseover) {
+            for (var i = 0; i < listeners.mouseover.length; i++) {
+                var listener = listeners.mouseover[i];
+                if (listener.passive === false) {
+                    oldHandler = listener.listener;
+                    element.removeEventListener('mouseover', oldHandler, { passive: false });
+                    break;
+                }
+            }
+        }
     }
 
-    // Other conditions where you want to prevent the default action
-    event.preventDefault();
-});
+    // Polyfill for getEventListeners (for older browsers)
+    function getEventListeners(el) {
+        if (typeof getEventListeners !== 'function') {
+            return; // If the browser does not support this function
+        }
+        return getEventListeners(el);
+    }
+
+    // Remove old event listener
+    findAndRemoveOldListener();
+
+    // Add the new event listener
+    if (oldHandler) {
+        element.addEventListener('mouseover', oldHandler, { passive: true });
+    }
+})();
